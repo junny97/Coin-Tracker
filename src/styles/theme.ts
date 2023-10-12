9088e74d37a28c8ed4c16d05157cf5bab10d26a2
junny97 <pcohad12@gmail.com>
2023-10-12 14:33:16 +0900

    Style: ThemeProvider로 다크모드 테마 설정하기 #1

---
+++ src/styles/theme.ts
@@ -0,0 +1,7 @@
+import { DefaultTheme } from 'styled-components';
+//DefaultTheme로 정의한 interface 타입들을 theme.ts에서 사용
+export const theme: DefaultTheme = {
+  bgColor: '#2f3640',
+  textColor: '#f5f6fa',
+  accentColor: '#44bd32',
+};
