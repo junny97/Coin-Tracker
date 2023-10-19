bd32bbb2a5f7a03e6c25d80b8b7b8c9f80fd8ae4
junny97 <pcohad12@gmail.com>
2023-10-19 21:15:06 +0900

    Refactor: globalStyle에 import한 폰트  index.html 파일에서 import하는 방식으로 변경#1

--- src/styles/globalStyle.tsx
+++ src/styles/globalStyle.tsx
@@ -2,7 +2,7 @@ import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
 
-@import url('https://fonts.googleapis.com/css2?family=Source+Sans+3:wght@300&display=swap');
+
 
 html, body, div, span, applet, object, iframe,
 h1, h2, h3, h4, h5, h6, p, blockquote, pre,
