7e145b6f22da0e374d03ea9ab166dc1af6dcccf9
junny97 <pcohad12@gmail.com>
2023-10-20 16:28:38 +0900

    Feat: 차트 데이터 받아오기 & UI 꾸미기#5

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,7 +1,83 @@
-export default function Chart() {
+import { useQuery } from '@tanstack/react-query';
+import { useParams } from 'react-router-dom';
+import { fetchCoinHistory } from '../api/api';
+import ApexChart from 'react-apexcharts';
+interface ChartProps {
+  coinId: string;
+}
+
+interface IHistorical {
+  time_open: string;
+  time_close: string;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  market_cap?: number;
+}
+
+export default function Chart({ coinId }: ChartProps) {
+  const { isLoading, data } = useQuery<IHistorical[]>({
+    queryKey: ['ohlcv', coinId],
+    queryFn: () => fetchCoinHistory(coinId),
+    // refetchInterval: 5000,
+    // refetchIntervalInBackground: true,
+  });
+
   return (
     <div>
-      <h1>chart</h1>
+      {isLoading ? (
+        'Loading chart...'
+      ) : (
+        <ApexChart
+          type='line'
+          series={[
+            {
+              name: 'Price',
+              data: data?.map((price) => price.close) ?? [],
+            },
+          ]}
+          options={{
+            theme: {
+              mode: 'dark',
+            },
+            chart: {
+              height: 300,
+              width: 500,
+              toolbar: {
+                show: false,
+              },
+              background: 'transparent',
+            },
+            grid: { show: false },
+            stroke: {
+              curve: 'smooth',
+              width: 4,
+            },
+            yaxis: {
+              show: false,
+            },
+            xaxis: {
+              axisBorder: { show: false },
+              axisTicks: { show: false },
+              labels: { show: false },
+              type: 'datetime',
+              categories: data?.map((price) => price.time_close),
+            },
+            fill: {
+              type: 'gradient',
+              gradient: { gradientToColors: ['#0be881'], stops: [0, 100] },
+            },
+            colors: ['#0fbcf9'],
+            tooltip: {
+              y: {
+                formatter: (value) => `$${value.toFixed(2)}`,
+              },
+            },
+          }}
+        />
+      )}
     </div>
   );
 }
