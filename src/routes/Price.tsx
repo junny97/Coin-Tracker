6e1a87cbf82cc7a08eccba01be6f62a264c21c16
junny97 <pcohad12@gmail.com>
2023-10-21 20:36:20 +0900

    Feat: price 컴포넌트 생성 & 가격 정보들 받아오기 & ui 스타일링 #7

--- src/routes/Price.tsx
+++ src/routes/Price.tsx
@@ -1,7 +1,47 @@
-export default function Price() {
+import styled from 'styled-components';
+
+const PriceContainer = styled.div`
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 10px 20px;
+  border-radius: 10px;
+`;
+
+const PriceList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+`;
+
+const List = styled.li`
+  cursor: pointer;
+  &:hover {
+    color: ${(props) => props.theme.accentColor};
+  }
+`;
+
+interface PriceProps {
+  price?: number | string;
+  maxPrice?: number | string;
+  maxDate?: string;
+  oneWeekDate?: number;
+}
+
+export default function Price({
+  price,
+  maxPrice,
+  maxDate,
+  oneWeekDate,
+}: PriceProps) {
   return (
-    <div>
-      <h1>price</h1>
-    </div>
+    <>
+      <PriceContainer>
+        <PriceList>
+          <List>실시간 가격정보: {price} USD</List>
+          <List>7일간 평균가: {oneWeekDate} USD</List>
+          <List>최고가 갱신일: {maxDate} </List>
+          <List>최고가: {maxPrice} USD </List>
+        </PriceList>
+      </PriceContainer>
+    </>
   );
 }
