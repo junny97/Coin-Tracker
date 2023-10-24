f24ca236c3b757501fb26723b207dcb488cbce7f
junny97 <pcohad12@gmail.com>
2023-10-24 14:35:55 +0900

    Refactor: 가격 정보 UI 표 형식으로 변경  props 전달 방식이 아닌 직접 데이터 받아 처리하는 방식으로 변경#7

--- src/routes/Price.tsx
+++ src/routes/Price.tsx
@@ -1,47 +1,184 @@
+import React from 'react';
 import styled from 'styled-components';
+import { IPrice, ICoinId } from '../interface';
+import { useQuery } from '@tanstack/react-query';
+import { fetchCoinTickers } from '../api/api';
 
-const PriceContainer = styled.div`
+const Container = styled.div`
   background-color: ${(props) => props.theme.bgAccentColor};
-  padding: 10px 20px;
-  border-radius: 10px;
+  margin-top: 0.5em;
+  width: 100%;
+  border-radius: 1em;
+  padding: 1em 2em;
 `;
 
-const PriceList = styled.ul`
-  display: flex;
-  flex-direction: column;
-  gap: 15px;
+const Table = styled.table`
+  width: 100%;
+  border-collapse: collapse;
 `;
 
-const List = styled.li`
-  cursor: pointer;
-  &:hover {
-    color: ${(props) => props.theme.accentColor};
+const TableHeader = styled.th`
+  padding: 0.5em;
+  text-align: left;
+  font-weight: bold;
+  border-right: 1px solid #ddd;
+`;
+const SubTableHeader = styled(TableHeader)`
+  font-weight: normal;
+`;
+
+const TableRow = styled.tr`
+  border-bottom: 1px solid #ddd;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  padding: 0.5em;
+  margin-bottom: 0.05em;
+`;
+
+const TableCell = styled.td`
+  padding: 0.4em;
+  text-align: center;
+`;
+
+const SmallText = styled.span`
+  font-size: 14px;
+  text-align: center;
+`;
+
+interface PercentChangeCellProps {
+  isNegative: boolean;
+}
+
+const PercentChangeCell = styled(SmallText)<PercentChangeCellProps>`
+  color: ${(props) => (props.isNegative ? '#FF033E' : '#00AC28')};
+
+  &.main {
+    font-size: 16px;
+    padding: 0.5em;
   }
 `;
 
-interface PriceProps {
-  price?: number | string;
-  maxPrice?: number | string;
-  maxDate?: string;
-  oneWeekDate?: number;
-}
+export default function Price({ coinId }: ICoinId) {
+  const { isLoading, data } = useQuery<IPrice>({
+    queryKey: ['tickers', coinId],
+    queryFn: () => fetchCoinTickers(coinId),
+  });
 
-export default function Price({
-  price,
-  maxPrice,
-  maxDate,
-  oneWeekDate,
-}: PriceProps) {
   return (
-    <>
-      <PriceContainer>
-        <PriceList>
-          <List>실시간 가격정보: {price} USD</List>
-          <List>7일간 평균가: {oneWeekDate} USD</List>
-          <List>최고가 갱신일: {maxDate} </List>
-          <List>최고가: {maxPrice} USD </List>
-        </PriceList>
-      </PriceContainer>
-    </>
+    <Container>
+      {isLoading ? (
+        <span>Loading...</span>
+      ) : (
+        <Table>
+          <tbody>
+            <TableRow>
+              <TableHeader>Attribute</TableHeader>
+              <TableCell>Value</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableHeader>Price</TableHeader>
+              <TableCell>${data?.quotes.USD.price.toFixed(3)}</TableCell>
+              <SubTableHeader>
+                {' '}
+                <SmallText>Percent Change (1H)</SmallText>
+              </SubTableHeader>
+              <TableCell>
+                {' '}
+                <PercentChangeCell
+                  isNegative={
+                    data?.quotes.USD.percent_change_1h !== undefined &&
+                    data.quotes.USD.percent_change_1h < 0
+                  }
+                >
+                  {data?.quotes.USD.percent_change_1h}%
+                </PercentChangeCell>
+              </TableCell>
+              <SubTableHeader>
+                {' '}
+                <SmallText>Percent Change (24H)</SmallText>
+              </SubTableHeader>
+              <TableCell>
+                {' '}
+                <PercentChangeCell
+                  isNegative={
+                    data?.quotes.USD.percent_change_24h !== undefined &&
+                    data.quotes.USD.percent_change_24h < 0
+                  }
+                >
+                  {data?.quotes.USD.percent_change_24h}%
+                </PercentChangeCell>
+              </TableCell>
+              <SubTableHeader>
+                {' '}
+                <SmallText>Percent Change (7D) </SmallText>
+              </SubTableHeader>
+              <TableCell>
+                <PercentChangeCell
+                  isNegative={
+                    data?.quotes.USD.percent_change_7d !== undefined &&
+                    data.quotes.USD.percent_change_7d < 0
+                  }
+                >
+                  {data?.quotes.USD.percent_change_7d}%
+                </PercentChangeCell>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableHeader>Market Capitalization</TableHeader>
+              <TableCell>${data?.quotes.USD.market_cap.toFixed(3)}</TableCell>
+              <SubTableHeader>
+                {' '}
+                <SmallText>Percent Change (24H) </SmallText>
+              </SubTableHeader>
+              <TableCell>
+                <PercentChangeCell
+                  isNegative={
+                    data?.quotes.USD.market_cap_change_24h !== undefined &&
+                    data.quotes.USD.market_cap_change_24h < 0
+                  }
+                >
+                  {data?.quotes.USD.market_cap_change_24h}%
+                </PercentChangeCell>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableHeader>Trading Volume (24H)</TableHeader>
+              <TableCell>${data?.quotes.USD.volume_24h.toFixed(3)}</TableCell>
+              <SubTableHeader>
+                {' '}
+                <SmallText>Percent Change (24H)</SmallText>
+              </SubTableHeader>
+              <TableCell>
+                {' '}
+                <PercentChangeCell
+                  isNegative={
+                    data?.quotes.USD.volume_24h_change_24h !== undefined &&
+                    data.quotes.USD.volume_24h_change_24h < 0
+                  }
+                >
+                  {data?.quotes.USD.volume_24h_change_24h}%
+                </PercentChangeCell>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableHeader>All-Time High</TableHeader>
+              <TableCell>${data?.quotes.USD.ath_price.toFixed(3)}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableHeader>Price Now / All-Time High</TableHeader>
+              <PercentChangeCell
+                className='main'
+                isNegative={
+                  data?.quotes.USD.percent_from_price_ath !== undefined &&
+                  data.quotes.USD.percent_from_price_ath < 0
+                }
+              >
+                {data?.quotes.USD.percent_from_price_ath}%
+              </PercentChangeCell>
+            </TableRow>
+          </tbody>
+        </Table>
+      )}
+    </Container>
   );
 }
