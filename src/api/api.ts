e98f80b9a7360c4504b87e31837f86630a416219
junny97 <pcohad12@gmail.com>
2023-11-21 22:14:15 +0900

    Refactor: api 이름, 경로 수정#9

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const BASE_URL = 'https://api.coinpaprika.com/v1';
-export const fetchCoins = async () => {
+
+export const getCoinList = async () => {
   try {
     const response = await axios.get(`${BASE_URL}/coins`);
     return response.data;
@@ -10,19 +11,22 @@ export const fetchCoins = async () => {
   }
 };
 
-export const fetchCoinInfo = async (coinId: string) => {
-  const response = await axios.get(`${BASE_URL}/coins/${coinId}`);
-  return response.data;
+export const getCoinPriceData = async (coinId: string) => {
+  try {
+    const response = await axios.get(`${BASE_URL}/tickers/${coinId}`);
+    return response.data;
+  } catch (error) {
+    console.error('데이터 불러오기 실패', error);
+  }
 };
 
-export const fetchCoinTickers = async (coinId: string) => {
-  const response = await axios.get(`${BASE_URL}/tickers/${coinId}?quotes=USD`);
-  return response.data;
-};
-
-export const fetchCoinHistory = async (coinId: string) => {
-  const response = await axios.get(
-    `https://ohlcv-api.nomadcoders.workers.dev?coinId=${coinId}`
-  );
-  return response.data;
+export const getCoinChartData = async (coinId: string) => {
+  try {
+    const response = await axios.get(
+      `https://ohlcv-api.nomadcoders.workers.dev?coinId=${coinId}`
+    );
+    return response.data;
+  } catch (error) {
+    console.error('데이터 불러오기 실패', error);
+  }
 };
